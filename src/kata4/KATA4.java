894a48ba1cf41786f62a0ec7fa89e39a8505c5d9
usuario <usuario@10.22.143.55>
2016-11-04 11:03:04 +0000

    KATA 4 -Version 2
    Llamada recursiva. 
    Comprobacion de si es directorio o fichero y muestra tipo árbol

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -5,14 +5,31 @@ import java.io.File;
 public class KATA4 {
 
     public static void main(String[] args) {
-        
-    File file = new File("c:\\");
-    String names[] = file.list(); // listado directorios del fichero guardado en ese array
-    
-        for (String name : names) {
-            System.out.println(name);
-            
+
+        File file = new File("c:\\pub\\tmp");
+
+        // llamamos al procedimiento print
+        print(file.listFiles(), ""); //listFile -> array de ficheros
+    }
+
+    private static void print(File[] files, String indent) {
+
+        // si es vacia o nual que salga
+        if (files == null) {
+            return;
         }
-        
+        for (File file : files) {
+            //imprimimos el indent y comprobamos si es un directorio
+                // si es asi +, sino -
+            System.out.println(indent
+                    + (file.isDirectory() ? "+" : "-") + file.getName());
+
+            // si no es un directorio o está oculto que pase a la siguiente
+            if (!file.isDirectory() || file.isHidden()) {
+                continue;
+            }
+            //llamada recursiva
+            print(file.listFiles(), indent +"  ");
+        }
     }
 }
